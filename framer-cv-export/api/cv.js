638442efcb7593671f629533960bb73b345d3eac
studiolex <91504263+studiolex@users.noreply.github.com>
2026-06-10 15:08:12 +0200

    Update cv.js

--- framer-cv-export/api/cv.js
+++ framer-cv-export/api/cv.js
@@ -75,12 +75,12 @@ module.exports = async (req, res) => {
 async function generatePdf(url) {
   const chromium = require("@sparticuz/chromium");
   const puppeteer = require("puppeteer-core");
-
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath(),
-    headless: true,
-  });
+const browser = await puppeteer.launch({
+  args: chromium.args,
+  defaultViewport: chromium.defaultViewport,
+  executablePath: await chromium.executablePath(),
+  headless: chromium.headless,
+});
   try {
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle0", timeout: 30000 });
