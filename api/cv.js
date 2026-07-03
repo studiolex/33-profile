8a55c9b2685b319b9cde53222d752920ed75e087
studiolex <91504263+studiolex@users.noreply.github.com>
2026-07-03 13:15:32 +0700

    Update cv.js

--- api/cv.js
+++ api/cv.js
@@ -9,7 +9,7 @@ const PDF_OPTIONS = {
   format: "A4",
   printBackground: true,
   scale: 0.65,
-  margin: { top: "8mm", bottom: "8mm", left: "0mm", right: "0mm" },
+  margin: { top: "0mm", bottom: "0mm", left: "0mm", right: "0mm" },
 };
 
 module.exports = async (req, res) => {
